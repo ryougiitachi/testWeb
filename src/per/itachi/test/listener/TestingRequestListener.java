c13f581eb7dbcb07944b9bc86a39e3b221aa40f3
ryougiitachi <ryougiitachi@gmail.com>
2017-11-10 17:48:22 +0800

    Start to focus on jvm memory management

--- src/per/itachi/test/listener/TestingRequestListener.java
+++ src/per/itachi/test/listener/TestingRequestListener.java
@@ -23,10 +23,20 @@ public class TestingRequestListener implements ServletRequestListener {
 	public void requestInitialized(ServletRequestEvent event) {
 		logger.debug("Request {} has initialised. ", event.getServletRequest());
 //		System.out.println("Request {0} has initialised. ");
+		//check jvm settings
+		Runtime runtime = Runtime.getRuntime();
+		logger.debug("After initialising request, the max memory Xmx is {}", runtime.maxMemory());
+		logger.debug("After initialising request, the total memory(Xms) is {}", runtime.totalMemory());
+		logger.debug("After initialising request, the free memory(Xms) is {}", runtime.freeMemory());
 	}
 
 	@Override
 	public void requestDestroyed(ServletRequestEvent event) {
 		logger.debug("Request {} has destroyed. ", event.getServletRequest());
+		//check jvm settings
+		Runtime runtime = Runtime.getRuntime();
+		logger.debug("After destroying request, the max memory Xmx is {}", runtime.maxMemory());
+		logger.debug("After destroying request, the total memory(Xms) is {}", runtime.totalMemory());
+		logger.debug("After destroying request, the free memory(Xms) is {}", runtime.freeMemory());
 	}
 }
