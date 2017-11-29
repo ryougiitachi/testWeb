83a472680d61322ef7ace37e7cc5f2060e8ccffb
ryougiitachi <ryougiitachi@gmail.com>
2017-11-29 17:09:21 +0800

    Added notes and comments for rabbitmq

--- shell/RabbitMQ.bat
+++ shell/RabbitMQ.bat
@@ -6,19 +6,19 @@ REM 在$RABBITMQ_BASE/log下面的日志可以看到启动信息，里面有rabb
 rem rabbitmq基本管理命令
 rem 参考文章：https://www.cnblogs.com/s648667069/p/6401463.html
 rem 启动rabbitmq服务，在windows下默认会启动新建一个名为RabbitMQ的服务
-rabbitmq-service.bat start
-rabbitmq-service.bat stop
+#rabbitmq-service.bat start
+#rabbitmq-service.bat stop
 rem 创建虚拟主机vhostRabbit，重复添加会报错Error: vhost_already_exists: vhostRabbit
-rabbitmqctl add_vhost vhostRabbit
+#rabbitmqctl add_vhost vhostRabbit
 rem 添加用户，重复添加会报错Error: user_already_exists: testadmin
-rabbitmqctl add_user testadmin testadmin123
+#rabbitmqctl add_user testadmin testadmin123
 rem 为用户添加角色(管理员)，重复赋予不报错
-rabbitmqctl set_user_tags testadmin administrator
+#rabbitmqctl set_user_tags testadmin administrator
 rem 设置用户在虚拟主机上的权限
-rabbitmqctl set_permissions -p vhostRabbit testadmin ".*" ".*" ".*"
+#rabbitmqctl set_permissions -p vhostRabbit testadmin ".*" ".*" ".*"
 
 rem rabbitmq网页管理工具
 rem 默认情况下http://localhost:15672是其地址，默认用户名密码皆为guest，该工具会在安装rabbitmq的时候自动安装；
 rem 可以使用以下命令启用插件，默认是自动启用的(?)
-rabbitmq-plugins enable rabbitmq_management
+#rabbitmq-plugins enable rabbitmq_management
 
